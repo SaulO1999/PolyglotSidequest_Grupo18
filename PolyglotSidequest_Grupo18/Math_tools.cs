98d3c791341fb4fc745de273f1d75f41ae96e09d
hulyio <00039718@uca.edu.sv>
2021-05-11 17:36:29 -0600

    añadiendo mas funciones a math tools

--- PolyglotSidequest_Grupo18/Math_tools.cs
+++ PolyglotSidequest_Grupo18/Math_tools.cs
@@ -8,22 +8,73 @@ namespace PolyglotSidequest_Grupo18
 {
     public class Math_tools
     {
-        //List<List<float>> list = new List<List<float>>();
-
-        
         public void zeroes(ref Matrix<float> M, int n)
         {
             //Se crean n filas
             for (int i = 0; i < n; i++)
             {
                 //Se crea una fila de n ceros
-                Vector<float> row = Vector<float>.Build.Dense(n, float.Parse("0.0"));
+                Vector<float> row = Vector<float>.Build.Dense(n, (float)0.0);
                 //Vector<float> row = new Vector<float>(zero,n);
                 //Se ingresa la fila en la matriz
                 M.SetRow(i, row);
                 Console.WriteLine(M.ToString());
             }
         }
-    }
+
+        void zeroes(ref Vector<float> v, int n)
+        {
+            //Se itera n veces
+            for (int i = 0; i < n; i++)
+            {
+                //fill with zeroes
+                v.Add((float)0.0);
+            }
+        }
+
+        void copyMatrix(Matrix<float> A, ref Matrix<float> copy)
+        {
+            A.CopyTo(copy);
+        }
+
+        void productMatrixVector(Matrix<float> A, Vector<float> v,ref Vector<float> R)
+        {
+            for (int f = 0; f < A.RowCount; f++)
+            {
+                //Se inicia un acumulador
+                float cell = (float)0.0;
+                //Se calcula el valor de la celda de acuerdo a la formulación
+                for (int c = 0; c < v.Count; c++)
+                {
+                    cell += A.At(f,c) * v.At(c);
+                }
+                //Se coloca el valor calculado en su celda correspondiente en la respuesta
+                R[f] += cell;
+            }
+        }
+
+        void productRealMatrix(float real, Matrix<float> M, ref Matrix<float> R)
+        {
+            M.Multiply(real);
+            M.CopyTo(R);
+        }
+
+        void getMinor(Matrix<float> M, int i, int j)
+        {
+            //Se elimina la fila i
+            //M.erase(M.begin() + i); //Uso de begin para obtener un iterator a la posición de interés
+                                    //Se recorren las filas restantes
+            M.RemoveRow(i);
+            M.RemoveColumn(j);
+            //for (int i = 0; i < M.RowCount; i++)
+                //En cada fila se elimina la columna j
+                //M.at(i).erase(M.at(i).begin() + j);
+                
+        }
+
+        float determinant(Matrix M)
+        {
+
+        }
 }
 
