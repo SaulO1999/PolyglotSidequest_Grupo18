c4b851ac3bd5863d18500079711293a217593bd7
hulyio <00039718@uca.edu.sv>
2021-05-10 22:52:25 -0600

    adding math tools and trying to define a matrix

---
+++ PolyglotSidequest_Grupo18/Math_tools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+
+namespace PolyglotSidequest_Grupo18
+{
+    class Math_tools
+    {
+        readonly List<List<float>> list = new List<List<float>>();
+
+        list
+        void zeroes(ref Vector<Vector<float>> M, int n)
+        {
+            //Se crean n filas
+            for (int i = 0; i < n; i++)
+            {
+                //Se crea una fila de n ceros
+                Vector<float> row(n,0.0);
+                //Se ingresa la fila en la matriz
+                M.push_back(row);
+                
+            }
+        }
+    }
+}
+
---
+++ PolyglotSidequest_Grupo18/Sel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyglotSidequest_Grupo18
+{
+    class Sel
+    {
+
+    }
+}
