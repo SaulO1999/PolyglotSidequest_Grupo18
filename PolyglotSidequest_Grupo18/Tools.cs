25960eceaf628d4383493768afffbb2e9c73648d
hulyio <00039718@uca.edu.sv>
2021-05-11 21:06:31 -0600

    finished leer malla y condiciones

--- PolyglotSidequest_Grupo18/Tools.cs
+++ PolyglotSidequest_Grupo18/Tools.cs
@@ -10,36 +10,218 @@ namespace PolyglotSidequest_Grupo18
     {
         private const string FILE_NAME = "problem.msh";
 
-        public void obtenerDatos(ref StreamReader file, int nlines, int n, int mode, ref Node[] item_list)
+        public void obtenerDatos(ref StreamReader file, int itemcase, int n, int mode, ref Node[] item_list)
         {
             //Se prepara una variable string para leer las líneas a omitir
-            string line;
+            string line = "";
             string[] lineArray;
             //Siempre se omite al menos una línea
             //file >> line;
             Console.WriteLine("empiezanreadlines");
-            line = file.ReadLine();
-            lineArray = line.Split('\t');
-            Console.WriteLine(line);
-            lineArray = line.Split(' ');
-            Console.WriteLine(line);
-            line = file.ReadLine();
 
-            
-            //lineArray = line.Split('\t');
-            //Console.WriteLine(line);
-            //lineArray = line.Split(' ');
-            //Console.WriteLine(line);
-            //Si es necesario omitir una línea adicional, se efectúa
-            if (nlines == 2)
+            switch (itemcase)
             {
-                /*file >> line;*/
-                line = file.ReadLine();
+                case 0:
+                    while (line != "Coordinates")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                        break;
+                case 1:
+                    while (line != "Elements")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 2:
+                    while (line != "Dirichlet")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 3:
+                    while (line != "Neumann")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
             }
 
                 //Se itera una cantidad de veces igual a la cantidad de datos a extraer
                 //que será igual a la cantidad de objetos a instanciar
                 for (int i = 0; i < n; i++)
+            {
+                //Se verifica la cantidad y tipos de datos a extraer por fila
+                switch (mode)
+                {
+                    //Se extrae un entero y un real por fila
+                    case 1:
+                        int e; float r;
+                        line = file.ReadLine();
+                        lineArray = line.Split('\t');
+                        Console.WriteLine(lineArray);
+                        e = int.Parse(lineArray[0]);
+                        r = float.Parse(lineArray[1]);
+                        foreach (string item in lineArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        //file >> e >> r;
+                        //Se instancian el entero y el real del objeto actual
+                        item_list[i].setIntFloat(e, r);
+                        break;
+                    //Se extraen tres enteros
+                    case 2:
+                        int e1, e2, e3;
+                        line = file.ReadLine();
+                        lineArray = line.Split(' ');
+                        e1 = int.Parse(lineArray[0]);
+                        e2 = int.Parse(lineArray[1]);
+                        e3 = int.Parse(lineArray[2]);
+                        foreach (string item in lineArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        //file >> e1 >> e2 >> e3;
+                        //Se instancia los tres enteros en el objeto actual
+                        item_list[i].setIntIntInt(e1, e2, e3);
+                        break;
+                }
+            }
+        }
+
+        public void obtenerDatos(ref StreamReader file, int itemcase, int n, int mode, ref Element[] item_list)
+        {
+            //Se prepara una variable string para leer las líneas a omitir
+            string line = "";
+            string[] lineArray;
+            //Siempre se omite al menos una línea
+            //file >> line;
+            Console.WriteLine("empiezanreadlines");
+
+            switch (itemcase)
+            {
+                case 0:
+                    while (line != "Coordinates")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 1:
+                    while (line != "Elements")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 2:
+                    while (line != "Dirichlet")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 3:
+                    while (line != "Neumann")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+            }
+
+            //Se itera una cantidad de veces igual a la cantidad de datos a extraer
+            //que será igual a la cantidad de objetos a instanciar
+            for (int i = 0; i < n; i++)
+            {
+                //Se verifica la cantidad y tipos de datos a extraer por fila
+                switch (mode)
+                {
+                    //Se extrae un entero y un real por fila
+                    case 1:
+                        int e; float r;
+                        line = file.ReadLine();
+                        lineArray = line.Split('\t');
+                        Console.WriteLine(lineArray);
+                        e = int.Parse(lineArray[0]);
+                        r = float.Parse(lineArray[1]);
+                        foreach (string item in lineArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        //file >> e >> r;
+                        //Se instancian el entero y el real del objeto actual
+                        item_list[i].setIntFloat(e, r);
+                        break;
+                    //Se extraen tres enteros
+                    case 2:
+                        int e1, e2, e3;
+                        line = file.ReadLine();
+                        lineArray = line.Split(' ');
+                        e1 = int.Parse(lineArray[0]);
+                        e2 = int.Parse(lineArray[1]);
+                        e3 = int.Parse(lineArray[2]);
+                        foreach (string item in lineArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        //file >> e1 >> e2 >> e3;
+                        //Se instancia los tres enteros en el objeto actual
+                        item_list[i].setIntIntInt(e1, e2, e3);
+                        break;
+                }
+            }
+        }
+
+        public void obtenerDatos(ref StreamReader file, int itemcase, int n, int mode, ref Condition[] item_list)
+        {
+            //Se prepara una variable string para leer las líneas a omitir
+            string line = "";
+            string[] lineArray;
+            //Siempre se omite al menos una línea
+            //file >> line;
+            Console.WriteLine("empiezanreadlines");
+
+            switch (itemcase)
+            {
+                case 0:
+                    while (line != "Coordinates")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 1:
+                    while (line != "Elements")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 2:
+                    while (line != "Dirichlet")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                case 3:
+                    while (line != "Neumann")
+                    {
+                        line = file.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+            }
+
+            //Se itera una cantidad de veces igual a la cantidad de datos a extraer
+            //que será igual a la cantidad de objetos a instanciar
+            for (int i = 0; i < n; i++)
             {
                 //Se verifica la cantidad y tipos de datos a extraer por fila
                 switch (mode)
@@ -133,26 +315,46 @@ namespace PolyglotSidequest_Grupo18
             m.setSizes(nnodes, neltos, ndirich, nneu);
             m.createData();
             Node[] misnodos = m.getNodes();
-            //Console.WriteLine("empiezaforeach");
-            Console.WriteLine("before");
-            //foreach (Node item in misnodos)
-            //{
-            //    item.setIntFloat(0, (float)0.0);
-            //    Console.WriteLine(item);
-            //}
+            //Console.WriteLine("before");
             for(int i = 0; i < misnodos.Length; i++)
             {
                 Node item = new Node();
                 misnodos[i] = item;
                 misnodos[i].setIntFloat(0, (float)0.0);
-                Console.WriteLine(misnodos[i]);
+                //Console.WriteLine(misnodos[i]);
             }
-            Console.WriteLine("after");
+            //Console.WriteLine("after");
+            Element[] miselementos = m.getElements();
+            for (int i = 0; i < miselementos.Length; i++)
+            {
+                Element item = new Element();
+                miselementos[i] = item;
+                miselementos[i].setIntIntInt(0,0,0);
+                //Console.WriteLine(misnodos[i]);
+            }
+            Condition[] misdirichlet = m.getDirichlet();
+            //Console.WriteLine("before");
+            for (int i = 0; i < misdirichlet.Length; i++)
+            {
+                Condition item = new Condition();
+                misdirichlet[i] = item;
+                misdirichlet[i].setIntFloat(0, (float)0.0);
+                //Console.WriteLine(misnodos[i]);
+            }
+            Condition[] misneumann = m.getNeumann();
+            //Console.WriteLine("before");
+            for (int i = 0; i < misneumann.Length; i++)
+            {
+                Condition item = new Condition();
+                misneumann[i] = item;
+                misneumann[i].setIntFloat(0, (float)0.0);
+                //Console.WriteLine(misnodos[i]);
+            }
 
-            obtenerDatos(ref sr, 1, nnodes, 1, ref misnodos);
-            //obtenerDatos(file, DOUBLELINE, neltos, INT_INT_INT, m.getElements());
-            //obtenerDatos(file, DOUBLELINE, ndirich, INT_FLOAT, m.getDirichlet());
-            //obtenerDatos(file, DOUBLELINE, nneu, INT_FLOAT, m.getNeumann());
+            obtenerDatos(ref sr, 0, nnodes, 1, ref misnodos);
+            obtenerDatos(ref sr, 1, neltos, 2, ref miselementos);
+            obtenerDatos(ref sr, 2, ndirich, 1, ref misdirichlet);
+            obtenerDatos(ref sr, 3, nneu, 1, ref misneumann);
 
             /*
 
